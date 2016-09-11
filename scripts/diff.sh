0edc80a272eabf77f5d98dfa3c771180db7504f1
Pete Bachant <petebachant@gmail.com>
2016-09-11 09:58:39 -0400

    Fix diff if latest version is tagged

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -4,6 +4,13 @@
 
 PREVIOUS=$(git tag | tail -n1)
 CURRENT=$(git describe --always --dirty)
+
+# If current version is a tag, use one tag prior
+if [ $CURRENT = $PREVIOUS ]
+then
+    PREVIOUS=$(git tag | tail -n2 | head -n1)
+fi
+
 git show $PREVIOUS:paper.tex > archive/paper-$PREVIOUS.tex
 latexdiff --exclude-textcmd="section,subsection,subsubsection" --math-markup=off archive/paper-$PREVIOUS.tex paper.tex > paper-$PREVIOUS-$CURRENT-diff.tex
 latexmk paper-$PREVIOUS-$CURRENT-diff.tex -pdf -f
